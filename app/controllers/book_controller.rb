0ff92ac4242a238ef73f8f4c98634bf66875c0e7
Mukaddes <mukaddesbuyukkavut@gmail.com>
2014-05-13 03:08:21 +0300

    Form olusuyor, subjecler ve filelar listeleniyor

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,46 @@
+class BookController < ApplicationController
+  layout 'standard'
+  def list
+    @books = Book.all
+  end
+  def show
+    @book = Book.find(params[:id])
+  end
+  def new
+    @book = Book.new
+    @subjects = Subject.all
+  end
+  def create
+    @book = Book.new(book_params)
+    if @book.save!
+      redirect_to :action => 'list'
+    else
+      @subjects = Subject.all
+      render :action => 'new'
+    end
+  end
+  def edit
+    @book = Book.find(params[:id])
+    @subjects = Subject.all
+  end
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_params)
+      redirect_to :action => 'show', :id => @book
+    else
+      @subjects = Subject.all
+      render :action => 'edit'
+    end
+  end
+  def delete
+    Book.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :price, :description)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'book/list' => 'book#list'
+  get 'book/new' => 'book#new'
+  post 'book/create' => 'book#create'
+  get 'book/show' => 'book#show'
+  get 'book/show_subjects' => 'book#show_subjects'
+  get 'book/edit' => 'book#edit'
+  get 'book/delete' => 'book#delete'
+  post 'book/update' => 'book#update'
+end
